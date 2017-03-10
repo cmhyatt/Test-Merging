34bddbdfef0504798219b9298fe5396aca5be248
cmhyatt <cmhyatt@indiana.edu>
2017-03-10 15:02:14 -0500

    Initial Commit

---
+++ TestProjectMerge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProjectMerge
+//
+//  Created by Chris Hyatt on 3/10/17.
+//  Copyright © 2017 IU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
